6454c5400a00fc649d5ec679de2303314b524095
Carlos Hernandez Topete <72361463+toshiroto@users.noreply.github.com>
2022-03-16 07:09:51 +0900

    SetFullName Variable corrected

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -10,23 +10,24 @@ function App() {
     const { value, name } = event.target;
 
     setFullName((prevValue) => {
-      if (name === "fName") {
+      if (name === "fName")
         return {
           fName: value,
           lName: prevValue.lName
         };
-      } else if (name === "lName") {
+      else if (name === "lName") {
         return {
-          fName: prevValue.value,
+          fName: prevValue.fName,
           lName: value
         };
       }
     });
   }
+
   return (
     <div className="container">
       <h1>
-        Hello {fullName.fName} {fullName.lName}
+        Hello {fullName.fName} {fullName.lName}{" "}
       </h1>
       <form>
         <input
